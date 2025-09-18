140cea10f796ba7963535dcfc62ac856ac02f99c
hyewon <hyewon4782@naver.com>
2025-09-18 22:26:10 +0900

    백준 11659 파이썬

--- baekjoon/Python/bk_11659.py
+++ baekjoon/Python/bk_11659.py
@@ -2,15 +2,20 @@
 # 누적합 이용해보자
 # N개의 수를 넣을 배열 만들자
 
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# 누적합 배열 만들기
+# 누적합 배열
 prefix = [0] * (N+1)
 for i in range(1, N+1):
     prefix[i] = prefix[i-1] + arr[i-1]
 
-# M개의 구간에 대해 합 출력
+result = []
 for _ in range(M):
     i, j = map(int, input().split())
-    print(prefix[j] - prefix[i-1])
+    result.append(str(prefix[j] - prefix[i-1]))
+
+print("\n".join(result))
