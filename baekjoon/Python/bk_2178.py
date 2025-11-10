1818e5230d8802d758281293b959afe9939810f1
hyewon <hyewon4782@naver.com>
2025-11-10 16:59:22 +0900

    백준 2178 풀이중

---
+++ baekjoon/Python/bk_2178.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+board = []
+
+n, m = map(int, input().split()), 
+vis = [[False] * 502 for _ in range(502)]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1,0]
+
+Q = deque()
+vis[0][0] = True
+Q.append((0, 0))
+
+while Q:
+	cur = Q.popleft()
+	for dir in range(4):
+		nx = cur[0] + dx[dir]
+		ny = cur[1] + dy[dir]
+
+	if nx < 0 or nx >= n or ny < 0 or ny >= m:	
+		continue
+	if vis[nx][ny] or board[nx][ny] != 1:
+		continue
+	vis[nx][ny] = vis[cur[0]][cur[1]] + 1
+
+	Q.append((nx, ny))
+	
+for i in range(n):
+    for j in range(m):
+        print(vis[i][j] -1 , end = " ")
+    print()
